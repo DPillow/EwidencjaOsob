60a046f9294795401ba0e3c8c6971f7b10d39d62
DPillow <maciej192@gmail.com>
2023-07-06 19:00:07 +0200

    Add project files.

---
+++ EwidencjaOsob/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace EwidencjaOsob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Przywitanie
+            //Wybór akcji
+            ////a. dodanie osoby
+            ////b. usuniecie osoby
+            ////c. Wyszukanie danych osoby
+            ////d. szukanie po jednostce rej. (filtr)
+            MenuActionService actionService = new MenuActionService();
+            actionService = Initialize(actionService);
+
+            Console.WriteLine("Witam w programie ewidencji osób.");
+            Console.WriteLine("Proszę o wybranie odpowiedniej czynności:");
+
+            var mainMenu = actionService.GetMenuActionsByName("Main");
+            for (int i = 0; i < mainMenu.Count; i++)
+            {
+                Console.WriteLine($"{mainMenu[i].Id}. {mainMenu[i].Name}");
+            }
+            var operation = Console.ReadKey();
+
+
+
+
+
+
+        }
+        private static MenuActionService Initialize(MenuActionService actionService)
+        {
+            actionService.AddNewAction(1, "Dodaj osobę", "Main");
+            actionService.AddNewAction(2, "Usuń osobę", "Main");
+            actionService.AddNewAction(3, "Wyszukaj osobę", "Main");
+            actionService.AddNewAction(4, "Szukaj po jednostce rejestrowej", "Main");
+            return actionService;
+        }
+
+    }
+}
